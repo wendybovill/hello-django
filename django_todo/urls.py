0dec69016a2a0de086b2b33328ab74c2c4e4dd6c
wendybovill <wendybovill@gmail.com>
2023-06-26 21:44:26 +0000

    added tests

--- django_todo/urls.py
+++ django_todo/urls.py
@@ -35,4 +35,3 @@ urlpatterns = [
     path('toggle/<item_id>', views.toggle_item, name='toggle'),
     path('delete/<item_id>', views.delete_item, name='delete')
 ]
-
--- todo/forms.py
+++ todo/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Item
+from django.test import TestCase
 
 
 class ItemForm(forms.ModelForm):
