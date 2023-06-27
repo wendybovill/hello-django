1dc3182bf023db5cde2285dbaf762d5965a31478
wendybovill <wendybovill@gmail.com>
2023-06-27 19:48:49 +0000

    setup development environ

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -27,7 +27,7 @@ development = os.environ.get('DEVELOPMENT', False)
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get(
     'SECRET_KEY',
-    's*ktcib$#ssi)ligbb1o$seg1+5f)@ry48csfr!=64d_tcmz=')
+    '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
